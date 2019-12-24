1e4b24bff22f65936c26c537290b1f74799ef0b3
Pierre Szleper <szleper.pierre@gmail.com>
2019-12-24 01:35:13 +0100

    Now displays the correct names

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -8,7 +8,7 @@ class Business extends React.Component {
             <div className="image-container">
                 <img src={this.props.business.imageSrc} alt=''/>
             </div>
-            <h2>MarginOtto Pizzeria</h2>
+            <h2>{this.props.business.name}</h2>
             <div className="Business-information">
                 <div className="Business-address">
                     <p>{this.props.business.address}</p>
@@ -17,7 +17,7 @@ class Business extends React.Component {
                 </div>
                 <div className="Business-reviews">
                     <h3>{this.props.business.category}</h3>
-                    <h3 className="rating">{this.props.business.rating} stars}</h3>
+                    <h3 className="rating">{this.props.business.rating} stars</h3>
                     <p>{this.props.business.reviewCount} reviews</p>
                 </div>
             </div>
